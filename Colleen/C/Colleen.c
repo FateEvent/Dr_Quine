74d9f488fb4c444ea1c3398a2646ba922b1c32ab
faventur <faventur@student.42mulhouse.fr>
2024-11-22 21:26:08 +0100

    Colleen almost done (a function is missing)

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
-/* "Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation. */
-#define colleen(x)int main(){char *s="#include<stdio.h>%c/* %cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation. */%c#define colleen(x)int main(){char *s=%c%s%c;/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}%c%ccolleen(x)";/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}
-
-colleen(x)
+#include <stdio.h>
+/*
+	"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
+*/
+int	main(){
+	char *s="#include <stdio.h>%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%cint%cmain(){%c%cchar *s=%c%s%c;%c%c/*%c%c%cIf my grandmother had had wheels, she would have been a bike.%c%c*/%c%cprintf(s,10,10,9,34,34,10,10,9,10,9,34,s,34,10,9,10,9,9,10,9,10,9,10,9,10);%c%creturn(0);%c}";
+	/*
+		If my grandmother had had wheels, she would have been a bike.
+	*/
+	printf(s,10,10,9,34,34,10,10,9,10,9,34,s,34,10,9,10,9,9,10,9,10,9,10,9,10);
+	return(0);
+}
---
+++ Grace/C/Grace.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+/* "Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation. */
+#define colleen(x)int main(){char *s="#include<stdio.h>%c/* %cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation. */%c#define colleen(x)int main(){char *s=%c%s%c;/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}%c%ccolleen(x)";/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}
+
+colleen(x)
