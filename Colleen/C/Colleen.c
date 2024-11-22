a6ef6e95b46494d7f627edc46b79f0cbbfbade5e
faventur <faventur@student.42mulhouse.fr>
2024-11-22 22:21:32 +0100

    Colleen PERFECT!

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 /*
 	"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
 */
-int	main(){
-	char *s="#include <stdio.h>%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%cint%cmain(){%c%cchar *s=%c%s%c;%c%c/*%c%c%cIf my grandmother had had wheels, she would have been a bike.%c%c*/%c%cprintf(s,10,10,9,34,34,10,10,9,10,9,34,s,34,10,9,10,9,9,10,9,10,9,10,9,10);%c%creturn(0);%c}";
+
+void	print() {
+
+	char *s="#include <stdio.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%cvoid%cprint() {%c%c%cchar *s=%c%s%c;%c%cprintf(s,10,10,10,9,34,34,10,10,10,9,10,10,9,34,s,34,10,9,10,10,10,9,10,9,10,9,9,10,9,10,9,10,9,10);%c}%c%cint%cmain() {%c%c/*%c%c%cIf my grandmother had had wheels, she would have been a bike.%c%c*/%c%cprint();%c%creturn(0);%c}";
+	printf(s,10,10,10,9,34,34,10,10,10,9,10,10,9,34,s,34,10,9,10,10,10,9,10,9,10,9,9,10,9,10,9,10,9,10);
+}
+
+int	main() {
 	/*
 		If my grandmother had had wheels, she would have been a bike.
 	*/
-	printf(s,10,10,9,34,34,10,10,9,10,9,34,s,34,10,9,10,9,9,10,9,10,9,10,9,10);
+	print();
 	return(0);
 }
