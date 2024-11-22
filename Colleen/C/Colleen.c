c213b3d19c0e966d8f4ff6fe5a9e076d06357178
faventur <faventur@student.42mulhouse.fr>
2024-11-22 18:05:53 +0100

    Colleen in C done

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,3 +1,5 @@
-#define colleen(x)int main(){char *s="#include<stdio.h>%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation. %c*/%c#define printf(x)int main(){char *s=%c%s%c;printf(s,10,10,9,34,34,10,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}%c";printf(s,10,10,9,34,34,10,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}
+#include<stdio.h>
+/* "Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation. */
+#define colleen(x)int main(){char *s="#include<stdio.h>%c/* %cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation. */%c#define colleen(x)int main(){char *s=%c%s%c;/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}%c%ccolleen(x)";/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}
 
 colleen(x)
