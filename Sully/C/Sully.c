66d2af3e73b5709bbd6b3c589b96ad3cc748b7f8
faventur <faventur@student.42mulhouse.fr>
2024-11-23 17:58:05 +0100

    The program subtracts 1 from the number given in the #define and opens a file called Sully_n-1.c

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -7,10 +7,12 @@
 
 #define number "5"
 
-#define open(x)char *prog = "Sully_"; char *ext = ".c"; char filename[10]; snprintf(filename, 10, "%s%s%s", prog, number, ext); FILE *stream = fopen(filename, "w");
+#define checknum(x)char nb = number[0]; int next = nb - 48; if (next > 0) next -= 1; nb = next + 48;
 
-#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };
+#define open(x)char *prog = "Sully_"; char *ext = ".c"; char filename[10]; snprintf(filename, 10, "%s%c%s", prog, nb, ext); FILE *stream = fopen(filename, "w");
 
-#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define number %c5%c%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[10]; snprintf(filename, 10, %c%cs%cs%cs%c, prog, number, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define sully(x)int main(){ char *s=%c%s%c; open(x); print(x); fclose(stream); return(0); };%c%csully(x)"; open(x); print(x); fclose(stream); return(0); };
+#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };
+
+#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define number %c5%c%c%c#define checknum(x)char nb = number[0]; int next = nb - 48; if (next > 0) next -= 1; nb = next + 48;%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[10]; snprintf(filename, 10, %c%cs%cc%cs%c, prog, nb, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define sully(x)int main(){ char *s=%c%s%c; checknum(x); open(x); print(x); fclose(stream); return(0); };%c%csully(x)"; checknum(x); open(x); print(x); fclose(stream); return(0); };
 
 sully(x)
