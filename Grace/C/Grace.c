a58148fe40dcd418f96c9a6365cd570f7b136e7d
faventur <faventur@student.42mulhouse.fr>
2024-11-23 13:47:48 +0100

    Grace is PERFECT!

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -9,6 +9,6 @@
 
 #define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };
 
-#define grace(x)int main(){char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define open(x)FILE *stream = fopen(%c./Grace_kid.c%c, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define grace(x)int main(){char *s=%c%s%c;open(x);print(x);fclose(stream);return(0);}%c%cgrace(x)";open(x);print(x);fclose(stream);return(0);};
+#define grace(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define open(x)FILE *stream = fopen(%c./Grace_kid.c%c, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define grace(x)int main(){ char *s=%c%s%c; open(x); print(x); fclose(stream); return(0); };%c%cgrace(x)"; open(x); print(x); fclose(stream); return(0); };
 
 grace(x)
