99810dc5979e124726ac1c6d6caa97c4b2e07dc8
faventur <faventur@student.42mulhouse.fr>
2024-11-23 15:54:30 +0100

    Sully compiles

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -5,10 +5,12 @@
 	"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
 */
 
-#define open(x)FILE *stream = fopen("./Grace_kid.c", "w");
+#define number "5"
 
-#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };
+#define open(x)char *prog = "Sully_"; char *ext = ".c"; char filename[10]; snprintf(filename, 9, "%s%s%s", prog, number, ext); FILE *stream = fopen(filename, "w");
 
-#define grace(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define open(x)FILE *stream = fopen(%c./Grace_kid.c%c, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define grace(x)int main(){ char *s=%c%s%c; open(x); print(x); fclose(stream); return(0); };%c%cgrace(x)"; open(x); print(x); fclose(stream); return(0); };
+#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,34,34,34,34,34,25,25,25,34,34,34,10,10,10,10,34,s,34,10,10); };
 
-grace(x)
+#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define number %c5%c%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[10]; snprintf(filename, 9, %c%cs%cs%cs%c, prog, number, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,34,34,34,34,34,prog,number,ext,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define sully(x)int main(){ char *s=%c%s%c; open(x); print(x); fclose(stream); return(0); };%c%csully(x)"; open(x); print(x); fclose(stream); return(0); };
+
+sully(x)
