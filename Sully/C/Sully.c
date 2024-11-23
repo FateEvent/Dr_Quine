c7e40fcfff4bf6d4806121a99709698539d733d1
faventur <faventur@student.42mulhouse.fr>
2024-11-23 18:20:05 +0100

    the program modifies the number according to the subtraction and exits if it is 0; todo: transform the number string into an integer

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 /*
 	"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
@@ -7,12 +8,12 @@
 
 #define number "5"
 
-#define checknum(x)char nb = number[0]; int next = nb - 48; if (next > 0) next -= 1; nb = next + 48;
+#define checknum(x)char nb = number[0]; int next = nb - 48; if (next == 0) exit(0); next -= 1; nb = next + 48;
 
 #define open(x)char *prog = "Sully_"; char *ext = ".c"; char filename[10]; snprintf(filename, 10, "%s%c%s", prog, nb, ext); FILE *stream = fopen(filename, "w");
 
-#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };
+#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,10,9,34,34,10,10,10,34,nb,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };
 
-#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define number %c5%c%c%c#define checknum(x)char nb = number[0]; int next = nb - 48; if (next > 0) next -= 1; nb = next + 48;%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[10]; snprintf(filename, 10, %c%cs%cc%cs%c, prog, nb, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define sully(x)int main(){ char *s=%c%s%c; checknum(x); open(x); print(x); fclose(stream); return(0); };%c%csully(x)"; checknum(x); open(x); print(x); fclose(stream); return(0); };
+#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define number %c%c%c%c%c#define checknum(x)char nb = number[0]; int next = nb - 48; if (next == 0) exit(0); next -= 1; nb = next + 48;%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[10]; snprintf(filename, 10, %c%cs%cc%cs%c, prog, nb, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,10,10,10,10,34,34,34,34,34,37,37,37,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define sully(x)int main(){ char *s=%c%s%c; checknum(x); open(x); print(x); fclose(stream); return(0); };%c%csully(x)"; checknum(x); open(x); print(x); fclose(stream); return(0); };
 
 sully(x)
