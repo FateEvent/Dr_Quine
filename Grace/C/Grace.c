2c06de51c83c8318847abecaaafd27fcfd1a75de
faventur <faventur@student.42mulhouse.fr>
2024-11-23 13:45:38 +0100

    Grace compiles

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-/* "Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation. */
-#define colleen(x)int main(){char *s="#include<stdio.h>%c/* %cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation. */%c#define colleen(x)int main(){char *s=%c%s%c;/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}%c%ccolleen(x)";/* If my grandmother had had wheels, she would have been a bike. */printf(s,10,34,34,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10,10,10);return(0);}
+#include <stdio.h>
+#include <fcntl.h>
 
-colleen(x)
+/*
+	"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
+*/
+
+#define open(x)FILE *stream = fopen("./Grace_kid.c", "w");
+
+#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };
+
+#define grace(x)int main(){char *s="#include <stdio.h>%c#include <fcntl.h>%c%c/*%c%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%c%c#define open(x)FILE *stream = fopen(%c./Grace_kid.c%c, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,9,34,34,10,10,10,34,34,34,34,10,10,10,10,34,s,34,10,10); };%c%c#define grace(x)int main(){char *s=%c%s%c;open(x);print(x);fclose(stream);return(0);}%c%cgrace(x)";open(x);print(x);fclose(stream);return(0);};
+
+grace(x)
