3257d9419f31cce5aa4202a97e1818b7a7d21651
faventur <faventur@student.42mulhouse.fr>
2024-11-30 19:51:26 +0100

    in Sully (Rust) the number var changes according to the existence of the file Sully_5.rs

--- Sully/Rust/src/main.rs
+++ Sully/Rust/src/main.rs
@@ -1,24 +1,29 @@
+use std::path::Path;
 use std::fs::File;
 use std::io::Write;
+
+const NUMBER: i64 = 5;
 
 macro_rules! quote {
 	() => {
 		/*
-			Le pire n'est jamais certain.
+			This is enshittification: surpluses are first directed to users; then, once they're locked in, surpluses go to suppliers; then once they're locked in, the surplus is handed to shareholders and the platform becomes a useless pile of shit. From mobile app stores to Steam, from Facebook to Twitter, this is the enshittification lifecycle.
+			https://www.reddit.com/r/rust/comments/146y5y1/announcement_rrust_will_be_joining_the_blackout
 		*/
 	}
 }
 
-macro_rules! format {
+macro_rules! fmt_str {
 	() => {
-		"use std::fs::File;\nuse std::io::Write;\n\nmacro_rules! quote {{\n\t() => {{\n\t\t/*\n\t\t\tLe pire n'est jamais certain.\n\t\t*/\n\t}}\n}}\n\nmacro_rules! format {{\n\t() => {{\n\t\t{0:?}\n\t}}\n}}\n\nmacro_rules! open_and_write {{\n\t() => {{\n\t\tlet mut buffer = File::create(\"Grace_kid.rs\")?;\n\t\twrite!(&mut buffer, format!(), format!())?;\n\t}}\n}}\n\nmacro_rules! define_main {{\n\t($body:block) => {{\n\t\tfn main() -> std::io::Result<()> {{\n\t\t\t$body\n\t\t\tOk(())\n\t\t}}\n\t}};\n}}\n\ndefine_main!({{\n\tquote!();\n\topen_and_write!();\n}});"
+		"use std::path::Path;\nuse std::fs::File;\nuse std::io::Write;\n\nconst NUMBER: i64 = {1:?}\n\nmacro_rules! quote {{\n\t() => {{\n\t\t/*\n\t\t\tThis is enshittification: surpluses are first directed to users; then, once they're locked in, surpluses go to suppliers; then once they're locked in, the surplus is handed to shareholders and the platform becomes a useless pile of shit. From mobile app stores to Steam, from Facebook to Twitter, this is the enshittification lifecycle.\n\t\t\thttps://www.reddit.com/r/rust/comments/146y5y1/announcement_rrust_will_be_joining_the_blackout\n\t\t*/\n\t}}\n}}\n\nmacro_rules! fmt_str {{\n\t() => {{\n\t\t{0:?}\n\t}}\n}}\n\nmacro_rules! open_and_write {{\n\t($nbnext:expr) => {{\n\t\tlet filename = format!(\"Sully_{{}}.rs\", $nbnext);\n\t\tlet mut buffer = File::create(filename)?;\n\t\twrite!(&mut buffer, fmt_str!(), fmt_str!(), $nbnext)?;\n\t}}\n}}\n\nmacro_rules! define_main {{\n\t($body:block) => {{\n\t\tfn main() -> std::io::Result<()> {{\n\t\t\t$body\n\t\t\tOk(())\n\t\t}}\n\t}};\n}}\n\ndefine_main!({{\n\tquote!();\n\tlet mut nbnext: i64 = NUMBER;\n\tif Path::new(\"Sully_5.rs\").exists() {{\n\t\tnbnext = NUMBER - 1;\n\t}}\n\tif nbnext >= 0 {{\n\t\topen_and_write!(nbnext);\n\t}}\n}});"
 	}
 }
 
 macro_rules! open_and_write {
-	() => {
-		let mut buffer = File::create("Grace_kid.rs")?;
-		write!(&mut buffer, format!(), format!())?;
+	($nbnext:expr) => {
+		let filename = format!("Sully_{}.rs", $nbnext);
+		let mut buffer = File::create(filename)?;
+		write!(&mut buffer, fmt_str!(), fmt_str!(), $nbnext)?;
 	}
 }
 
@@ -33,5 +38,11 @@ macro_rules! define_main {
 
 define_main!({
 	quote!();
-	open_and_write!();
+	let mut nbnext: i64 = NUMBER;
+	if Path::new("Sully_5.rs").exists() {
+		nbnext = NUMBER - 1;
+	}
+	if nbnext >= 0 {
+		open_and_write!(nbnext);
+	}
 });
