66d496f530e973bffde67aa7534ccb8a6ac6c243
faventur <faventur@student.42mulhouse.fr>
2024-11-30 17:41:44 +0100

    in Sully (Rust) I implemented the format string as a macro

--- Grace/Rust/src/main.rs
+++ Grace/Rust/src/main.rs
@@ -9,10 +9,16 @@ macro_rules! quote {
 	}
 }
 
+macro_rules! format {
+	() => {
+		"use std::fs::File;\nuse std::io::Write;\n\nmacro_rules! quote {{\n\t() => {{\n\t\t/*\n\t\t\tLe pire n'est jamais certain.\n\t\t*/\n\t}}\n}}\n\nmacro_rules! format {{\n\t() => {{\n\t\t{0:?}\n\t}}\n}}\n\nmacro_rules! open_and_write {{\n\t() => {{\n\t\tlet mut buffer = File::create(\"Grace_kid.rs\")?;\n\t\twrite!(&mut buffer, format!(), format!())?;\n\t}}\n}}\n\nmacro_rules! define_main {{\n\t($body:block) => {{\n\t\tfn main() -> std::io::Result<()> {{\n\t\t\t$body\n\t\t\tOk(())\n\t\t}}\n\t}};\n}}\n\ndefine_main!({{\n\tquote!();\n\topen_and_write!();\n}});"
+	}
+}
+
 macro_rules! open_and_write {
 	() => {
 		let mut buffer = File::create("Grace_kid.rs")?;
-		write!(&mut buffer, "use std::fs::File;\nuse std::io::Write;\n\nmacro_rules! quote {{\n\t() => {{\n\t\t/*\n\t\t\tLe pire n'est jamais certain.\n\t\t*/\n\t}}\n}}\n\nmacro_rules! open_and_write {{\n\t() => {{\n\t\tlet mut buffer = File::create(\"Grace_kid.rs\")?;\n\t\twrite!(&mut buffer, {0:?}, {0:?})?;\n\t}}\n}}\n\nmacro_rules! define_main {{\n\t($body:block) => {{\n\t\tfn main() -> std::io::Result<()> {{\n\t\t\t$body\n\t\t\tOk(())\n\t\t}}\n\t}};\n}}\n\ndefine_main!({{\n\tquote!();\n\topen_and_write!();\n}});", "use std::fs::File;\nuse std::io::Write;\n\nmacro_rules! quote {{\n\t() => {{\n\t\t/*\n\t\t\tLe pire n'est jamais certain.\n\t\t*/\n\t}}\n}}\n\nmacro_rules! open_and_write {{\n\t() => {{\n\t\tlet mut buffer = File::create(\"Grace_kid.rs\")?;\n\t\twrite!(&mut buffer, {0:?}, {0:?})?;\n\t}}\n}}\n\nmacro_rules! define_main {{\n\t($body:block) => {{\n\t\tfn main() -> std::io::Result<()> {{\n\t\t\t$body\n\t\t\tOk(())\n\t\t}}\n\t}};\n}}\n\ndefine_main!({{\n\tquote!();\n\topen_and_write!();\n}});")?;
+		write!(&mut buffer, format!(), format!())?;
 	}
 }
 
