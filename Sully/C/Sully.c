90878bd12cd24939ad5bf3b54b666020e1900e17
faventur <faventur@student.42mulhouse.fr>
2024-12-02 14:02:57 +0100

    took out vars prog and ext from Sully (C)

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -11,12 +11,12 @@ int number = 5;
 
 #define checknum(x)if (number <= 0) exit(0); int nbnext = number - 1; if (number == 5 && access("Sully_5.c", F_OK) == -1) nbnext = 5;
 
-#define open(x)char *prog = "Sully_"; char *ext = ".c"; char filename[15]; snprintf(filename, 15, "%s%d%s", prog, nbnext, ext); FILE *stream = fopen(filename, "w");
+#define open(x)char filename[15]; snprintf(filename, 15, "Sully_%d.c", nbnext); FILE *stream = fopen(filename, "w");
 
-#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,10,10,9,10,10,10,nbnext,10,10,34,34,10,10,34,34,34,34,34,34,34,34,10,10,10,10,34,34,10,10,34,s,34,10,10); };
+#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,10,10,9,10,10,10,nbnext,10,10,34,34,10,10,34,34,34,34,10,10,10,10,34,34,10,10,34,s,34,10,10); };
 
-#define exec(x)char command[60]; snprintf(command, 60, "gcc %s%d.c -o %s%d && chmod +x %s%d && ./%s%d",prog,nbnext,prog,nbnext,prog,nbnext,prog,nbnext); system(command);
+#define exec(x)char command[60]; snprintf(command, 60, "gcc Sully_%d.c -o Sully_%d && chmod +x Sully_%d && ./Sully_%d",nbnext,nbnext,nbnext,nbnext); system(command);
 
-#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c%c/*%c%cNon plus ultra.%c*/%c%cint number = %d;%c%c#define checknum(x)if (number <= 0) exit(0); int nbnext = number - 1; if (number == 5 && access(%cSully_5.c%c, F_OK) == -1) nbnext = 5;%c%c#define open(x)char *prog = %cSully_%c; char *ext = %c.c%c; char filename[15]; snprintf(filename, 15, %c%%s%%d%%s%c, prog, nbnext, ext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,10,10,9,10,10,10,nbnext,10,10,34,34,10,10,34,34,34,34,34,34,34,34,10,10,10,10,34,34,10,10,34,s,34,10,10); };%c%c#define exec(x)char command[60]; snprintf(command, 60, %cgcc %%s%%d.c -o %%s%%d && chmod +x %%s%%d && ./%%s%%d%c,prog,nbnext,prog,nbnext,prog,nbnext,prog,nbnext); system(command);%c%c#define sully(x)int main(){ char *s=%c%s%c; checknum(x); open(x); print(x); fclose(stream); exec(x); return(0); };%c%csully(x)"; checknum(x); open(x); print(x); fclose(stream); exec(x); return(0); };
+#define sully(x)int main(){ char *s="#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c%c/*%c%cNon plus ultra.%c*/%c%cint number = %d;%c%c#define checknum(x)if (number <= 0) exit(0); int nbnext = number - 1; if (number == 5 && access(%cSully_5.c%c, F_OK) == -1) nbnext = 5;%c%c#define open(x)char filename[15]; snprintf(filename, 15, %cSully_%%d.c%c, nbnext); FILE *stream = fopen(filename, %cw%c);%c%c#define print(x)if (stream) { fprintf(stream,s,10,10,10,10,10,10,9,10,10,10,nbnext,10,10,34,34,10,10,34,34,34,34,10,10,10,10,34,34,10,10,34,s,34,10,10); };%c%c#define exec(x)char command[60]; snprintf(command, 60, %cgcc Sully_%%d.c -o Sully_%%d && chmod +x Sully_%%d && ./Sully_%%d%c,nbnext,nbnext,nbnext,nbnext); system(command);%c%c#define sully(x)int main(){ char *s=%c%s%c; checknum(x); open(x); print(x); fclose(stream); exec(x); return(0); };%c%csully(x)"; checknum(x); open(x); print(x); fclose(stream); exec(x); return(0); };
 
 sully(x)
